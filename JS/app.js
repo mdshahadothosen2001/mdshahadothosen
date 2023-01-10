84f3339382bd0aa460adddbf4d71c0b3eb22d80e
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-01-10 22:30:29 +0600

    init
    add files and folders

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+const tabs=document.querySelectorAll('[data-target]'),
+      tabContents=document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab=>{
+    tab.addEventListener('click',()=>{
+        const target=document.querySelector(tab.dataset.target)
+        tabContents.forEach(tc=>{
+            tc.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+
+
+
+    })
+})      
